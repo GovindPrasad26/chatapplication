e230c25cdd9f5da8b75cd4829f731469b2efcd18
22A91F0013 <danduproluprasad5@gmail.com>
2025-06-25 23:05:34 +0530

    Updated login and signup components

--- chatdude-client/client/src/signup/sign.jsx
+++ chatdude-client/client/src/signup/sign.jsx
@@ -1,12 +1,235 @@
 // import { useState } from "react";
-import "./sign.css";
+// import "./sign.css"
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import Login from "../login/login";
+// function SignUp() {
+//     let Navigatef = useNavigate()
+// const [error, setError] = useState(false);
+// const [validationErrors,setValidationErrors]=useState({})
+//   const [user, setUser] = useState({
+//     fname: "",
+//     lname: "",
+//     gender: "",
+//     email: "",
+//     password: "",
+//     phone: "",
+//   });
+//   const Validate =()=>{
+//     const error ={}
+//     if(!user.fname.trim()) error.fname="FIRST NAME REQIRED"
+//     if(!user.lname.trim()) error.lname="LAST NAME REQIRED"
+//     if(!user.gender.trim()) error.lname="GENDER SPECIFIED"
+//     if(!user.email.trim()){
+//     error.email="EMAIL REQUIRED"}
+//     else if (!/\S+@\S+\.\S+/.test(user.email)){
+//            error.email='IVALID FORMAT'
+//     }
+//    if(!user.password.trim()){
+//     error.password='PASSWORD IS WRONG'
+//    }else if(user.password.length<6){
+//        error.password='password must be at least 6 characters'
+//    }
+//    if(!user.phone.trim()){
+//     error.phone='phone number is required'}
+//         else if(!/^\d{10}$/.test(user.phone)){
+//             error.phone="phone number must be 10digits"
+//         }
+
+    
+//     return error
+//     }
+//   console.log(user)
+//   const getNewUser = () => {
+
+//       const errors = Validate()
+//       if(Object.keys(errors).length > 0){
+//          setValidationErrors(errors)
+//          return
+//       }
+
+
+//     axios
+//       .post("http://localhost:5665/users/signup", user)
+//       .then((res) => {
+//         if (res.data.ok) {
+     
+//           Navigatef('/login')
+//         }
+//         console.log(res);
+//         setUser({
+//           fname: "",
+//           lname: "",
+//           gender: "",
+//           email: "",
+//           password: "",
+//           phone: "",
+//         });
+//         setValidationErrors({});
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         setError(true);
+//       });
+//   };
+//   // const getNewUser = () => {
+
+//   //     const errors = Validate()
+//   //     if(Object.keys(errors).length > 0){
+//   //        setValidationErrors(errors)
+//   //        return
+//   //     }
+
+
+//   //   axios
+//   //     .post("http://localhost:5665/users/signup", user)
+//   //     .then((res) => {
+//   //       if (res.data.ok) {
+     
+//   //         Navigatef('/login')
+//   //       }
+//   //       console.log(res);
+//   //       setUser({
+//   //         fname: "",
+//   //         lname: "",
+//   //         gender: "",
+//   //         email: "",
+//   //         password: "",
+//   //         phone: "",
+//   //       });
+//   //       setValidationErrors({});
+//   //     })
+//   //     .catch((error) => {
+//   //       console.log(error);
+//   //       setError(true);
+//   //     });
+//   // };
+
+//   return (
+//     <div className="main-signup-container">
+//       <div className="signup-container">
+//         <h2>Signup Page</h2>
+//         <form>
+//           <div>
+//             <input
+//               onChange={(event) => {
+//                 setUser({ ...user, fname: event.target.value });
+//               }}
+//               type="text"
+//               value={user.fname}
+//               placeholder="First Name"
+//             />{validationErrors.fname && <p className="error-text">{validationErrors.fname}</p>}
+//           </div>
+//           <div>
+//             <input
+//               onChange={(event) => {
+//                 setUser({ ...user, lname: event.target.value });
+//               }}
+//               type="text"
+//               value={user.lname}
+//               placeholder="Last Name"
+//             />{validationErrors.lname && <p className="error-text">{validationErrors.lname}</p>}
+//           </div>
+//           <div style={{ display:'flex',marginLeft:'60px'}}>
+//             <div>
+//               <input
+//                 type="radio"
+//                 onChange={(event) => {
+//                   setUser({ ...user, gender: event.target.value });
+//                 }}
+//                 name="gender"
+//                 value={"male"}
+//                 checked={user.gender === "male"}
+
+//               />
+//               <label>male</label>
+//             </div>
+//             <div>
+//               <input
+//                 type="radio"
+//                 onChange={(event) => {
+//                   setUser({ ...user, gender: event.target.value });
+//                 }}
+//                 name="gender"
+//                 value={"female"}
+//                 checked={user.gender === "female"}
+
+//               />
+//               <label>female</label>
+//             </div>
+//             <div>
+//               <input
+//                 type="radio"
+//                 onChange={(event) => {
+//                   setUser({ ...user, gender: event.target.value });
+//                 }}
+//                 name="gender"
+//                 value={"other"} 
+//                 checked={user.gender === "other"}
+
+//               />{validationErrors.gender && <p className="error-text">{validationErrors.gender}</p>}
+//               <label>other</label>
+//             </div>
+//           </div>
+//           <div>
+//             <input
+//               type="email"
+//               onChange={(event) => {
+//                 setUser({ ...user, email: event.target.value });
+//               }}
+//               placeholder="Email Id"
+//               value={user.email}
+//             />{validationErrors.email && <p className="error-text">{validationErrors.email}</p>}
+//           </div>
+//           <div>
+//             <input
+//               type="password"
+//               onChange={(event) => {
+//                 setUser({ ...user, password: event.target.value });
+//               }}
+//               placeholder="Password"
+//               value={user.password}
+//             />{validationErrors.password && <p className="error-text">{validationErrors.password}</p>}
+//           </div>
+//           <div>
+//             <input
+//               type="text"
+//               onChange={(event) => {
+//                 setUser({ ...user, phone: event.target.value });
+//               }}
+//               placeholder="Mobile Number"
+//               value={user.phone}
+//             />{validationErrors.phone && <p className="error-text">{validationErrors.phone}</p>}
+//           </div>
+//           <div>
+//             <input type="button" value="signup" onClick={getNewUser} />
+//           </div>
+//         </form>
+//       </div>
+
+//       {error ? (
+//         <div style={{ textAlign: "center" }}>
+//           <h2 style={{ color: "red", fontSize: "28px" }}>
+//             Failed to Create Account
+//           </h2>
+//         </div>
+//       ) : null}
+//     </div>
+//   );
+// }
+
+// export default SignUp;
+
+
+import { useState } from "react";
+import "./sign.css"
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
+import Login from "../login/login";
 function SignUp() {
-  const Navigatef = useNavigate();
-  const [error, setError] = useState(false);
-  const [validationErrors, setValidationErrors] = useState({});
+    let Navigatef = useNavigate()
+const [error, setError] = useState(false);
+const [validationErrors,setValidationErrors]=useState({})
   const [user, setUser] = useState({
     fname: "",
     lname: "",
@@ -14,52 +237,47 @@ function SignUp() {
     email: "",
     password: "",
     phone: "",
-    image: "", // added image field
   });
+  const Validate =()=>{
+    const error ={}
+    if(!user.fname.trim()) error.fname="FIRST NAME REQIRED"
+    if(!user.lname.trim()) error.lname="LAST NAME REQIRED"
+    if(!user.gender.trim()) error.lname="GENDER SPECIFIED"
+    if(!user.email.trim()){
+    error.email="EMAIL REQUIRED"}
+    else if (!/\S+@\S+\.\S+/.test(user.email)){
+           error.email='IVALID FORMAT'
+    }
+   if(!user.password.trim()){
+    error.password='PASSWORD IS WRONG'
+   }else if(user.password.length<6){
+       error.password='password must be at least 6 characters'
+   }
+   if(!user.phone.trim()){
+    error.phone='phone number is required'}
+        else if(!/^\d{10}$/.test(user.phone)){
+            error.phone="phone number must be 10digits"
+        }
 
-  const Validate = () => {
-    const error = {};
-    if (!user.fname.trim()) error.fname = "FIRST NAME REQUIRED";
-    if (!user.lname.trim()) error.lname = "LAST NAME REQUIRED";
-    if (!user.gender.trim()) error.gender = "GENDER REQUIRED";
-    if (!user.email.trim()) {
-      error.email = "EMAIL REQUIRED";
-    } else if (!/\S+@\S+\.\S+/.test(user.email)) {
-      error.email = "INVALID EMAIL FORMAT";
+    
+    return error
     }
-    if (!user.password.trim()) {
-      error.password = "PASSWORD REQUIRED";
-    } else if (user.password.length < 6) {
-      error.password = "Password must be at least 6 characters";
-    }
-    if (!user.phone.trim()) {
-      error.phone = "PHONE NUMBER REQUIRED";
-    } else if (!/^\d{10}$/.test(user.phone)) {
-      error.phone = "Phone number must be 10 digits";
-    }
-    return error;
-  };
-
+  console.log(user)
   const getNewUser = () => {
-    const errors = Validate();
-    if (Object.keys(errors).length > 0) {
-      setValidationErrors(errors);
-      return;
-    }
 
-    // Set default image if not provided
-    const finalUser = {
-      ...user,
-      image: user.image.trim()
-        ? user.image
-        : "https://cdn-icons-png.flaticon.com/512/149/149071.png", // default user icon
-    };
+      const errors = Validate()
+      if(Object.keys(errors).length > 0){
+         setValidationErrors(errors)
+         return
+      }
+
 
     axios
-      .post("https://cha-rsyf.onrender.com/users/signup", finalUser)
+      .post("https://cha-rsyf.onrender.com/users/signup", user)
       .then((res) => {
         if (res.data.ok) {
-          Navigatef("/login");
+     
+          Navigatef('/login')
         }
         console.log(res);
         setUser({
@@ -69,7 +287,6 @@ function SignUp() {
           email: "",
           password: "",
           phone: "",
-          image: "",
         });
         setValidationErrors({});
       })
@@ -78,6 +295,38 @@ function SignUp() {
         setError(true);
       });
   };
+  // const getNewUser = () => {
+
+  //     const errors = Validate()
+  //     if(Object.keys(errors).length > 0){
+  //        setValidationErrors(errors)
+  //        return
+  //     }
+
+
+  //   axios
+  //     .post("http://localhost:5665/users/signup", user)
+  //     .then((res) => {
+  //       if (res.data.ok) {
+     
+  //         Navigatef('/login')
+  //       }
+  //       console.log(res);
+  //       setUser({
+  //         fname: "",
+  //         lname: "",
+  //         gender: "",
+  //         email: "",
+  //         password: "",
+  //         phone: "",
+  //       });
+  //       setValidationErrors({});
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       setError(true);
+  //     });
+  // };
 
   return (
     <div className="main-signup-container">
@@ -86,120 +335,112 @@ function SignUp() {
         <form>
           <div>
             <input
+              onChange={(event) => {
+                setUser({ ...user, fname: event.target.value });
+              }}
               type="text"
-              placeholder="First Name"
               value={user.fname}
-              onChange={(e) => setUser({ ...user, fname: e.target.value })}
-            />
-            {validationErrors.fname && <p className="error-text">{validationErrors.fname}</p>}
+              placeholder="First Name"
+            />{validationErrors.fname && <p className="error-text">{validationErrors.fname}</p>}
           </div>
-
           <div>
             <input
+              onChange={(event) => {
+                setUser({ ...user, lname: event.target.value });
+              }}
               type="text"
-              placeholder="Last Name"
               value={user.lname}
-              onChange={(e) => setUser({ ...user, lname: e.target.value })}
-            />
-            {validationErrors.lname && <p className="error-text">{validationErrors.lname}</p>}
+              placeholder="Last Name"
+            />{validationErrors.lname && <p className="error-text">{validationErrors.lname}</p>}
           </div>
-
-          <div style={{ display: "flex", marginLeft: "60px" }}>
+          <div style={{ display:'flex',marginLeft:'60px'}}>
             <div>
               <input
                 type="radio"
+                onChange={(event) => {
+                  setUser({ ...user, gender: event.target.value });
+                }}
                 name="gender"
-                value="male"
+                value={"male"}
                 checked={user.gender === "male"}
-                onChange={(e) => setUser({ ...user, gender: e.target.value })}
+
               />
-              <label>Male</label>
+              <label>male</label>
             </div>
             <div>
               <input
                 type="radio"
+                onChange={(event) => {
+                  setUser({ ...user, gender: event.target.value });
+                }}
                 name="gender"
-                value="female"
+                value={"female"}
                 checked={user.gender === "female"}
-                onChange={(e) => setUser({ ...user, gender: e.target.value })}
+
               />
-              <label>Female</label>
+              <label>female</label>
             </div>
             <div>
               <input
                 type="radio"
+                onChange={(event) => {
+                  setUser({ ...user, gender: event.target.value });
+                }}
                 name="gender"
-                value="other"
+                value={"other"} 
                 checked={user.gender === "other"}
-                onChange={(e) => setUser({ ...user, gender: e.target.value })}
-              />
-              <label>Other</label>
+
+              />{validationErrors.gender && <p className="error-text">{validationErrors.gender}</p>}
+              <label>other</label>
             </div>
           </div>
-          {validationErrors.gender && <p className="error-text">{validationErrors.gender}</p>}
-
           <div>
             <input
               type="email"
+              onChange={(event) => {
+                setUser({ ...user, email: event.target.value });
+              }}
               placeholder="Email Id"
               value={user.email}
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-            />
-            {validationErrors.email && <p className="error-text">{validationErrors.email}</p>}
+            />{validationErrors.email && <p className="error-text">{validationErrors.email}</p>}
           </div>
-
           <div>
             <input
               type="password"
+              onChange={(event) => {
+                setUser({ ...user, password: event.target.value });
+              }}
               placeholder="Password"
               value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-            />
-            {validationErrors.password && <p className="error-text">{validationErrors.password}</p>}
+            />{validationErrors.password && <p className="error-text">{validationErrors.password}</p>}
           </div>
-
           <div>
             <input
               type="text"
+              onChange={(event) => {
+                setUser({ ...user, phone: event.target.value });
+              }}
               placeholder="Mobile Number"
               value={user.phone}
-              onChange={(e) => setUser({ ...user, phone: e.target.value })}
-            />
-            {validationErrors.phone && <p className="error-text">{validationErrors.phone}</p>}
+            />{validationErrors.phone && <p className="error-text">{validationErrors.phone}</p>}
           </div>
-
           <div>
-            <input
-              type="text"
-              placeholder="Image URL"
-              value={user.image}
-              onChange={(e) => setUser({ ...user, image: e.target.value })}
-            />
-          </div>
-
-          {user.image && (
-            <div style={{ marginTop: "10px" }}>
-              <img
-                src={user.image}
-                alt="Preview"
-                style={{ width: "100px", height: "100px", borderRadius: "50%" }}
-              />
-            </div>
-          )}
-
-          <div>
-            <input type="button" value="Signup" onClick={getNewUser} />
+            <input type="button" value="signup" onClick={getNewUser} />
           </div>
         </form>
       </div>
 
-      {error && (
+      {error ? (
         <div style={{ textAlign: "center" }}>
-          <h2 style={{ color: "red", fontSize: "28px" }}>Failed to Create Account</h2>
+          <h2 style={{ color: "red", fontSize: "28px" }}>
+            Failed to Create Account
+          </h2>
         </div>
-      )}
+      ) : null}
     </div>
   );
 }
 
 export default SignUp;
+
+
